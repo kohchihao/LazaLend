7fb09b42b7d45f8a83fc752416c7635f10061bc2
Jin Ying <developer.tjy@gmail.com>
2018-09-10 23:29:37 +0800

    categoriesInsert.sql: Add 1 insert query

---
+++ sql/categoriesInsert.sql
@@ -0,0 +1,3 @@
+SET search_path TO lazalend;
+
+INSERT INTO categories (name, image_url) VALUES ('Books', 'https://images.unsplash.com/photo-1526243741027-444d633d7365?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7491eb281478f3b97661bae38cbcb34b&auto=format&fit=crop&w=1951&q=80');
