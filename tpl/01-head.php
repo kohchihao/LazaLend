3541664d3b88d653ef085032fb0fc9da9aca0548
Jin Ying <developer.tjy@gmail.com>
2018-09-12 17:36:43 +0800

    Set up routing for page

---
+++ tpl/01-head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang = "en">
+    <!-- BEGIN HEAD -->
+    <head>
+        <meta charset="utf-8" />
+        <title><?=$_M['HEAD']['TITLE']; ?></title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="" name="description" />
+        <meta content="" name="author" />
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+        <!-- Page Level CSS -->
+        <?=$_M['HEAD']['CSS']?>
+
+        <!-- Shared CSS -->
+        <link rel="stylesheet" href="/css/ll.css?v=1">
+    </head>
+    <!-- END HEAD -->
+
+    <body>
+        <div class = "container">
