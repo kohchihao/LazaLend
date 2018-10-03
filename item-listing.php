a2d51fd926fcb6de2ebf36950d8862ce0e16b8a7
Jin Ying <developer.tjy@gmail.com>
2018-10-03 18:24:42 +0800

    Update item-listing styles

--- item-listing.php
+++ item-listing.php
@@ -1,7 +1,7 @@
 <?php
     $root = __DIR__."/";
     require_once $root."cfg.php";
-    require_once $root . "./template/template.php";
+    require_once $root . "./template/template_jy.php";
 
     $item_id = $_GET['id'];
 
@@ -16,23 +16,27 @@
     // End of Update Items
 
     // Item Details
-    $query = 'SELECT id, user_id, name, description, fee, pickup_lat, pickup_long, return_lat, return_long, date_available, borrowed
-      FROM items WHERE id = ' . $item_id;
-    $go_q = pg_equery($query);
-    $item_details = array();
-    $item_details = pg_fetch_assoc($go_q);
-    // Item Images
-    $query = 'SELECT image_link, cover FROM item_images WHERE item_id = ' . $item_id;
-    $go_q = pg_equery($query);
-    $item_images = array();
-    while ($fe_q = pg_fetch_assoc($go_q)) {
-        if ($fe_q['cover'] == 'TRUE') {
-            array_unshift($item_images, $fe_q['image_link']);
-        } else {
-            array_push($item_images, $fe_q['image_link']);
-        }
-    }
+    // $query = 'SELECT id, user_id, name, description, fee, pickup_lat, pickup_long, return_lat, return_long, date_available, borrowed
+    //   FROM items WHERE id = ' . $item_id;
+    // $go_q = pg_equery($query);
+    // $item_details = array();
+    // $item_details = pg_fetch_assoc($go_q);
+    // // Item Images
+    // $query = 'SELECT image_link, cover FROM item_images WHERE item_id = ' . $item_id;
+    // $go_q = pg_equery($query);
+    // $item_images = array();
+    // while ($fe_q = pg_fetch_assoc($go_q)) {
+    //     if ($fe_q['cover'] == 'TRUE') {
+    //         array_unshift($item_images, $fe_q['image_link']);
+    //     } else {
+    //         array_push($item_images, $fe_q['image_link']);
+    //     }
+    // }
     // End of Item Details
+
+    // Item Details
+    $item_details = getItem($item_id);
+    // End of item details
 
     $_M = Array(
         'HEAD' => Array (
@@ -59,7 +63,7 @@ require $root."template/01-head.php";
                 <div id="item-image-carousel" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
                         <?php
-                        foreach ($item_images as $image_index=>$image_link) {
+                        foreach ($item_details['image_link'] as $image_index=>$image_link) {
                             echo '<li data-target="#item-image-carousel" data-slide-to="'.$image_index.'"'
                                     .($image_index==0?' class="active"':'').'></li>';
                         }
@@ -67,9 +71,9 @@ require $root."template/01-head.php";
                     </ol>
                     <div class="carousel-inner">
                         <?php
-                        foreach ($item_images as $image_index=>$image_link) {
+                        foreach ($item_details['image_link'] as $image_index=>$image_link) {
                             echo '<div class = "carousel-item'.($image_index==0?' active':'').'">
-                                <img class="d-block w-100 img-carousel" src = ".'.$image_link.'" />
+                                    <img class="img-carousel" src = ".'.$image_link.'" alt = "Item Image"/>
                                 </div>';
                         }
                         ?>
@@ -83,45 +87,88 @@ require $root."template/01-head.php";
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
+
                 <!-- only show delete option if owner is logged in -->
                 <?php if (isset($_SESSION['loggedInUserId']) && $_SESSION['loggedInUserId'] == $item_details['user_id']) {?>
-                    <a href="delete-item.php?id=<?=$item_id?>">Delete Listing</a>
+                    <div class = "mt-4 mb-5 del-btn-container"> 
+                        <a href="delete-item.php?id=<?=$item_id?>" class = "btn btn-danger delete-btn">Delete Listing</a>
+                    </div>
                 <?php } ?>
 
             </div>
-            <div class = "col-md-6">
-                <div class = "row">
-                    <div class = "h3"><?=$item_details['name'] ?></div>
-                </div>
-                <!-- only show edit option if owner is logged in -->
-                <?php if (isset($_SESSION['loggedInUserId']) && $_SESSION['loggedInUserId'] == $item_details['user_id']) {?>
-                    <a href="item-dashboard.php?id=<?=$item_id?>">Edit Listing</a>
+            <div class = "col-md-6 left-div">
+                <section class = "detail-container">
+                    <h4><?=$item_details['name'] ?></h4>
+
+                    <div class = "mt-4 item-action">
+                        <!-- only show edit option if owner is logged in -->
+                        <?php if (isset($_SESSION['loggedInUserId']) && $_SESSION['loggedInUserId'] == $item_details['user_id']) {?>
+                            <a href="item-dashboard.php?id=<?=$item_id?>" class = "btn blue-btn">Edit Listing</a>
+                        <?php } else { ?>
+                            <a href = "bid?id=<?=$item_id?>" class = "btn blue-btn">Make Offer</a>
+                        <?php } ?>
+                    </div>
+
+                    <div class = "row mt-3 mb-3">
+                        <div class = "col-md-4  description-label"><i class="fas fa-tag"></i></div>
+                        <div class = "col-md-8">S$<?=$item_details['fee'] ?>/day</div>
+                    </div>
+                    <div class = "row mt-3 mb-3description-label">
+                        <div class = "col-md-4  description-label"><i class="fas fa-info-circle"></i></div>
+                        <div class = "col-md-8"><?=$item_details['description'] ?></div>
+                    </div>
+                    <div class = "row mt-3 mb-3">
+                        <div class = "col-md-4  description-label"><i class="fas fa-align-justify"></i></div>
+                        <div class = "col-md-8">In <a href = "categories?id=<?=$item_details['category_id']?>"><?=$item_details['category']?></a></div>
+                    </div>
+                    <div class = "row mt-3 mb-3">
+                        <div class = "col-md-4  description-label">Date Available:</div>
+                        <div class = "col-md-8"><?=$item_details['date_available'] ?></div>
+                    </div>
+                </section>
+
+                <section class = "mt-4 addresses detail-container">
+                    <h4 class = "owner-header">Loaning This</h4>
+
+                    <div class = "row mt-3 mb-3">
+                        <div class = "col-md-4  description-label">Pickup Location:</div>
+                        <div class = "col-md-8">
+                            <a href = "https://www.google.com/maps/place/<?=$item_details['pickup_lat']?>, <?=$item_details['pickup_long']?>" target = "_blank">
+                                <?=$item_details['pickup_address']?>
+                            </a>
+                        </div>
+                    </div>
+                    <div class = "row my-3">
+                        <div class = "col-md-4  description-label">Return Location:</div>
+                        <div class = "col-md-8">
+                            <a href = "https://www.google.com/maps/place/<?=$item_details['return_lat']?>, <?=$item_details['return_long']?>" target = "_blank">
+                                <?=$item_details['return_address']?>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+
+                <?php if (isset($_SESSION['loggedInUserId']) && $_SESSION['loggedInUserId'] != $item_details['user_id']) {?>
+                    <section class = "mt-4 detail-container">
+                        <h4 class = "owner-header">Meet the Owner</h4>
+
+                        <figure class = "user-info">
+                            <img src = ".<?=$item_details['user_profile_image']?>" alt = "Profile Picture"/>
+                            <figcaption class = "user-info-details">
+                                <summary>
+                                    <a href = "#" class = "username"><?=$item_details['username']?></a>
+                                </summary>
+                                <div class = "user-details">
+                                    <label class = "user-joined"><?=$item_details['user_joined']?></label>
+                                </div>
+                            </figcaption>
+                        </figure>
+                    </section>
                 <?php } ?>
-                <div class = "row mt-5 mb-3">
-                    <div class = "col-md-4">Description:</div>
-                    <div class = "col-md-8"><?=$item_details['description'] ?></div>
-                </div>
-                <div class = "row mt-3 mb-5">
-                    <div class = "col-md-4">Fee:</div>
-                    <div class = "col-md-8">$<?=$item_details['fee'] ?>/day</div>
-                </div>
-                <div class = "row mt-5 mb-5">
-                    <div class = "col-md-4">Date Available:</div>
-                    <div class = "col-md-8"><?=$item_details['date_available'] ?></div>
-                </div>
-                <div class = "row mt-5 mb-3">
-                    <div class = "col-md-4">Pickup Location:</div>
-                    <div class = "col-md-8">WIP</div>
-                </div>
-                <div class = "row my-3">
-                    <div class = "col-md-4">Return Location:</div>
-                    <div class = "col-md-8">WIP</div>
-                </div>
             </div>
         </div>
     </section>
 
 <?php
-    require_once $root . "./template/modal.php";
     require $root."template/footer.php";
 ?>
--- template/template_jy.php
+++ template/template_jy.php
@@ -50,4 +50,95 @@
 
         return false;
     }
+
+    function getItem($item_id) {
+        // TODO: Remove the require
+        require_once "template.php";
+
+        $pickup_address = "";
+        $return_address = "";
+        $base_url = "https://" . MAPS_HOST . "/maps/api/geocode/json?key=" . GMAPS_API_KEY;
+        $lat_lng_url = $base_url . "&latlng=";
+        
+        $query = 'SELECT 
+            it.id, 
+            it.user_id, 
+            it.name, 
+            u.username,
+            u.profile_image_url,
+            u.created AS user_created,
+            it.description, 
+            it.fee, 
+            it.pickup_lat, 
+            it.pickup_long, 
+            it.return_lat, 
+            it.return_long, 
+            it.date_available, 
+            ii.image_link,
+            ii.cover,
+            c.name AS category,
+            c.id AS category_id
+        FROM items it, item_images ii, categories c, users u
+        WHERE it.id = ' . $item_id . '
+        AND ii.item_id = it.id 
+        AND it.category_id = c.id
+        AND it.user_id = u.id';
+        
+        $go_q = pg_equery($query);
+
+        $item = Array();
+        $item['image_link'] = Array();
+
+        while($fe_q = pg_fetch_assoc($go_q)) {
+            $item['id'] = $fe_q['id'];
+            $item['user_id'] = $fe_q['user_id'];
+            $item['name'] = $fe_q['name'];
+            $item['username'] = $fe_q['username'];
+            $item['user_profile_image'] = $fe_q['profile_image_url'];
+            $item['user_joined'] = get_time_ago($fe_q['user_created']);
+            $item['description'] = $fe_q['description'];
+            $item['fee'] = $fe_q['fee'];
+            $item['date_available'] = $fe_q['date_available'];
+            $item['category'] = $fe_q['category'];
+            $item['category_id'] = $fe_q['category_id'];
+            $item['pickup_lat'] = $fe_q['pickup_lat'];
+            $item['pickup_long'] = $fe_q['pickup_long'];
+            $item['return_lat'] = $fe_q['return_lat'];
+            $item['return_long'] = $fe_q['return_long'];
+
+            
+            // Handle Images
+            if ($fe_q['cover'] == t) {
+                array_unshift($item['image_link'], $fe_q['image_link']);
+            } else {
+                array_push($item['image_link'], $fe_q['image_link']);
+            }
+        }
+
+         // Handle Location
+         $pickup_url = $lat_lng_url . $item['pickup_lat'] . ",%20" . $item['pickup_long'];
+         $return_url = $lat_lng_url . $item['return_lat'] . ",%20" . $item['return_long'];
+         $pickup_address = latlongToAddress($pickup_url);
+         $return_address = latlongToAddress($return_url);
+         
+         $item['pickup_address'] = $pickup_address;
+         $item['return_address'] = $return_address;
+
+        return $item;
+    }
+
+    function latlongToAddress($url) {
+        $address = "";
+        
+        $json = file_get_contents($url);
+        $obj = json_decode($json);
+
+        $status = $obj->status;
+
+        if($status == "OK") {
+            $address = $obj->results[0]->address_components[2]->short_name;
+        }
+
+        return $address;
+    }
 ?>
