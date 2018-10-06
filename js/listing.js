8d80b488d32fbadc6101ea30b5b79336dc90e2ec
Jin Ying <developer.tjy@gmail.com>
2018-10-07 00:26:57 +0800

    Create Bids Feature

--- js/listing.js
+++ js/listing.js
@@ -57,14 +57,3 @@ function back_to_loan_category() {
     $('#loan-categories').show();
     $('#loan-details').show();
 }
-
-function show_error(error_msg) {
-    new Noty({
-        type: 'error', //alert (default), success, error, warning, info - ClassName generator uses this value → noty_type__${type}
-        layout: 'topRight', //top, topLeft, topCenter, topRight (default), center, centerLeft, centerRight, bottom, bottomLeft, bottomCenter, bottomRight - ClassName generator uses this value → noty_layout__${layout}
-        theme: 'bootstrap-v4', //relax, mint (default), metroui - ClassName generator uses this value → noty_theme__${theme}
-        text: error_msg, //This string can contain HTML too. But be careful and don't pass user inputs to this parameter.
-        timeout: 3000, // false (default), 1000, 3000, 3500, etc. Delay for closing event in milliseconds (ms). Set 'false' for sticky notifications.
-        progressBar: true, //Default, progress before fade out is displayed
-    }).show();
-}   
--- template/template_jy.php
+++ template/template_jy.php
@@ -1,62 +1,5 @@
-<?php 
-    function isNotLoggedIn() {
-        if (!isset($_SESSION['loggedInUserId'])) {
-            echo 'disabled';
-        } 
-    }
-    
-    function no_image_selected($image_id) {      
-        $html = '<div id = "image_'.$image_id.'">
-                    <label for="loan_images_'.$image_id.'" class="loan-thumb">
-                        <div class="loan-thumb-content">
-                            <i class="far fa-image"></i>';
-                            if($image_id == 1) $html .= '<span class = "loan-cover">Cover</span>';
-                        $html.= '</div>
-                    </label>
-                </div>
-                <input type="file" name="loan_images[]" id="loan_images_'.$image_id.'" class = "image_input hidden">';
-
-        return $html;
-    }
-
-    function image_selected($image_id, $image_url) {
-        $html = '
-            <div class = "loan-thumb">
-                <div class = "loan-thumb-content">
-                    <img src = "'.$image_url.'">';
-                    if($image_id == 1) $html .= '<span class = "loan-cover">Cover</span>';
-                    $html .= '<button type = "button" class = "btn del-loan-images" id = "del_loan_image_'.$image_id.'" onclick="del_loan_image(this)"><i class="far fa-trash-alt"></i></button>
-                </div>
-            </div>
-        ';
-
-        return $html;
-    }
-
-    function isValidImage($target_file) {
-        $image_file_type = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            return false;
-        }
-
-        // Allow certain file formats
-        if($image_file_type != "jpg" && $image_file_type != "png" && $image_file_type != "jpeg") {
-            return false;
-        }
-
-        return true;
-    }
-
-    function isEmptyField($field) {
-        if($field == "") {
-            return true;
-        }
-
-        return false;
-    }
-
+<?php
+    /* ================ Page Queries ==================== */
     function getItem($item_id) {
         $pickup_address = "";
         $return_address = "";
@@ -130,6 +73,119 @@
         return $item;
     }
 
+    function getItemBids($item_id) {
+        $bids = Array(); 
+
+        $query = "SELECT it.fee, MIN(b.bid_price) AS min_bid, MAX(b.bid_price) AS max_bid
+            FROM  items it, bids b
+            WHERE it.id = ".$item_id."
+            AND b.item_id = it.id
+            GROUP BY it.id, it.fee
+            ";
+
+        $go_q = pg_query($query);
+
+        $bids = pg_fetch_assoc($go_q);
+
+        return $bids;
+    }
+
+    function getUserBid($user_id, $item_id) {
+        $query = "SELECT bid_price, duration_of_loan, date_of_loan FROM bids WHERE bidder_id = " . $user_id . " AND item_id = " . $item_id;
+        $go_q = pg_query($query);
+        $user_bid = pg_fetch_assoc($go_q);
+
+        return $user_bid;
+    }
+
+    function createBid($bidder_id, $item_id, $bid_day, $bid_fee, $date_of_loan) {
+        $query = "SELECT user_id FROM items WHERE id = " . $item_id;   
+        $go_q = pg_query($query);
+        $owner_id = pg_fetch_assoc($go_q)['user_id'];
+
+        if(empty($owner_id)) {
+            return false;
+        }
+
+        $insert = "INSERT INTO bids (owner_id, bidder_id, item_id, bid_price, duration_of_loan, date_of_loan)
+            VALUES (".$owner_id.", ".$bidder_id.", ".$item_id.", ".pg_escape_string($bid_fee).", ".pg_escape_string($bid_day).", '".pg_escape_string($date_of_loan)."')
+            ON CONFLICT (owner_id, bidder_id, item_id) DO UPDATE 
+            SET bid_price = excluded.bid_price,
+            duration_of_loan = excluded.duration_of_loan,
+            date_of_loan = excluded.date_of_loan";
+
+        $go_i = pg_query($insert);
+
+        if(pg_affected_rows($go_i) <= 0) {
+            return false;
+        }
+
+        return true;
+    }
+    /* ================ End of Page Queries ==================== */
+
+
+    /* ================ Create Items Page ==================== */    
+    function no_image_selected($image_id) {      
+        $html = '<div id = "image_'.$image_id.'">
+                    <label for="loan_images_'.$image_id.'" class="loan-thumb">
+                        <div class="loan-thumb-content">
+                            <i class="far fa-image"></i>';
+                            if($image_id == 1) $html .= '<span class = "loan-cover">Cover</span>';
+                        $html.= '</div>
+                    </label>
+                </div>
+                <input type="file" name="loan_images[]" id="loan_images_'.$image_id.'" class = "image_input hidden">';
+
+        return $html;
+    }
+
+    function image_selected($image_id, $image_url) {
+        $html = '
+            <div class = "loan-thumb">
+                <div class = "loan-thumb-content">
+                    <img src = "'.$image_url.'">';
+                    if($image_id == 1) $html .= '<span class = "loan-cover">Cover</span>';
+                    $html .= '<button type = "button" class = "btn del-loan-images" id = "del_loan_image_'.$image_id.'" onclick="del_loan_image(this)"><i class="far fa-trash-alt"></i></button>
+                </div>
+            </div>
+        ';
+
+        return $html;
+    }
+    /* ================ End of Create Items Page ==================== */
+
+    /* ================ Utils ==================== */
+    function isNotLoggedIn() {
+        if (!isset($_SESSION['loggedInUserId'])) {
+            echo 'disabled';
+        } 
+    }
+    
+    function isValidImage($target_file) {
+        $image_file_type = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            return false;
+        }
+
+        // Allow certain file formats
+        if($image_file_type != "jpg" && $image_file_type != "png" && $image_file_type != "jpeg") {
+            return false;
+        }
+
+        return true;
+    }
+
+    function isEmptyField($field) {
+        if($field == "") {
+            return true;
+        }
+
+        return false;
+    }
+
     function latlongToAddress($url) {
         $address = "";
         
@@ -175,4 +231,5 @@
 
         return $string_diff;
     }
+    /* ================ End of Utils ==================== */
 ?>
